15b4720aa5fbdfa659dcb7a6bfc0cb321e2495b9
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-02-10 10:05:04 +0530

    Add files via upload

---
+++ starpattern.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void Starpattern(){
+	      for(int i=0;i<5;i++){
+          for(int j=0;j<i;j++){
+          printf("*");
+      }
+      printf("\n");
+      }
+	}
