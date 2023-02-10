ed1620444d5cca1ed5693adcb69a24252dec4d72
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-02-10 09:41:46 +0530

    Add files via upload

---
+++ prime no.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void IsPrime(){
+	int num,i;
+	int count=0;
+	printf("Enter a number: ");
+	scanf(" %d",&num);
+	if (num>1){
+		for(i=2;i<num;i++){
+			if(num%i==0){
+				count++;
+			}
+		}
+	}
+	if (count>0){
+		printf("%d is a Prime Number!",num);
+	}
+	else{
+		printf("%d is not a Prime Number!",num);
+	}
+	}
+
