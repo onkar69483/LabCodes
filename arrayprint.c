74f586f246e6f370c8f54ed44ad9352851181203
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-03-24 10:12:07 +0530

    Add files via upload

---
+++ arrayprint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+  
+int arrayprint()  
+{  
+   int i,arr[10];
+   for(i=0;i<10;i++){
+   	scanf("%d",&arr[i]);
+   }
+   for(i=0;i<10;i++){
+   	printf("%d ",arr[i]);
+   }
+  
+     return 0;  
+}  
