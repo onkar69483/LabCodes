146d6bc738b4ef62636e1e69957e62b5cfe29d77
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-07-24 10:16:28 +0530

    Update searchAlgo.c

--- OnkarDsl/searchAlgo.c
+++ OnkarDsl/searchAlgo.c
@@ -11,6 +11,17 @@ int linearSearch(int arr[],int target){
 	return -1;
 }
 
+int recursiveLinSearch(int arr[],int i,int target){
+	
+	if(arr[i]==target){
+		return i;
+	}else if(9<i+1){
+		return -1;
+	}
+	else{
+		return recursiveLinSearch(arr,i+1,target);
+	}
+}
 int binarySearch(int arr[],int target){
 	int left = 0, right = 9,mid;
 	while(left<=right){
@@ -28,6 +39,23 @@ int binarySearch(int arr[],int target){
 	return -1;
 }
 
+int recursiveBinSearch(int arr[],int low,int high,int target){
+	int mid;
+	while(low<=high){
+		mid=(low+high)/2;
+		if(arr[mid]==target){
+			return mid;
+		}
+		else if(target>arr[mid]){
+			return recursiveBinSearch(arr,mid+1,high,target);
+		}
+		else{
+			return recursiveBinSearch(arr,low,mid-1,target);
+		}
+	}
+	return -1;
+}
+
 int main() {
 	int arr[10],i,target,num;
 	printf("Enter elements of an integer array: ");
@@ -36,11 +64,11 @@ int main() {
 	}
 	printf("\nEnter the element to search in the array: ");
 	scanf("%d",&target);
-	printf("\n1. Linear Search\n2. Binary Search\nPress Key: ");
+	printf("\n1. Linear Search\n2. Binary Search\n3. Recursive Binary Search\n4. Recursive Linear Search\nPress Key: ");
 	scanf("%d",&num);
 	switch(num){
 		case 1:
-	if(binarySearch(arr,target)==-1){
+	if(linearSearch(arr,target)==-1){
 		printf("\nTarget not found!");
 	}
 	else{
@@ -55,6 +83,22 @@ int main() {
 		printf("\nTarget found at index: %d",binarySearch(arr,target));
 	}
 	break;
+	case 3:
+	if(recursiveBinSearch(arr,0,9,target)==-1){
+		printf("\nTarget not found!");
+	}
+	else{
+		printf("\nTarget found at index: %d",recursiveBinSearch(arr,0,9,target));
+	}
+	break;	
+	case 4:
+	if(recursiveLinSearch(arr,0,target)==-1){
+		printf("\nTarget not found!");
+	}
+	else{
+		printf("\nTarget found at index: %d",recursiveLinSearch(arr,0,target));
+	}
+	break;	
     default: 
     printf("Invalid Input!");
 }		
