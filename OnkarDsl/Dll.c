d0ddabc81926a8e0350ade1e725ad1619ec09ead
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-09-11 10:19:08 +0530

    Update Dll.c

--- OnkarDsl/Dll.c
+++ OnkarDsl/Dll.c
@@ -9,6 +9,10 @@ struct node{
 
 struct node *head,*tail=NULL;
 
+//create new linkedlist
+    struct node *head1,*tail1=NULL;
+
+
 void create(){
     struct node *newnode = (struct node*)malloc(sizeof(struct node));
     printf("\nEnter data: ");
@@ -205,11 +209,62 @@ void sort(){
         p=p->next;
     }
 }
+
+void reverse(){
+	struct node *p = NULL,*p1;
+	while(head!=NULL){
+	p1=head;
+	head=head->next;
+	p1->prev=p1->next;
+	p1->next = p;
+	p=p1;	
+	}
+	head=p1;	
+}
+
+void concatenate(struct node *ptr, struct node *ptr1){
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    ptr->next=ptr1;
+}
+
+struct node *intersection(struct node *ptr, struct node *ptr1) {
+    struct node *result = NULL; 
+    struct node *p = ptr; 
+    while (p != NULL) {
+        struct node *q = ptr1;
+        while (q != NULL) {
+            if (p->data == q->data) {
+                struct node *newNode = (struct node *)malloc(sizeof(struct node));
+                newNode->data = p->data;
+                result->prev=newNode;
+                newNode->next = result;
+                result = newNode;
+                break; 
+            }
+            q = q->next;
+        }
+        p = p->next;
+    }
+    return result; 
+}
+
 void main(){
     int ops;
+    struct node *newnode1 = (struct node*)malloc(sizeof(struct node));
+    newnode1->data = 55;
+    newnode1->next=head1;
+    head1=tail1=newnode1;
+    struct node *newnode2 = (struct node*)malloc(sizeof(struct node));
+    newnode2->data = 56;
+    head1->prev=newnode2;
+    newnode2->next=head1;
+    head1=newnode2;
+    
     while(ops!=7){
     printf("\n1) Create linked List \n2) Display List \n3) insert at begin \n4) Insert at end \n5) insert at postion \n6) insert after postion \n7) Exit \n");
-    printf("8) deleteatbegin\n9) deleteatend\n10) deleteatpos\n11) deleteafterpos\n12) search\n13) Sort\n");
+    printf("8) deleteatbegin\n9) deleteatend\n10) deleteatpos\n11) deleteafterpos\n12) search\n13) Sort\n14) Reverse\n");
     printf("Enter the choice ");
     scanf("%d",&ops);
     switch(ops){
@@ -251,6 +306,15 @@ void main(){
         case 13:
             sort();
             break;
+        case 14:
+            reverse();
+            break;
+        case 15:
+            concatenate(head,head1);
+            break;
+        case 16:
+            head = intersection(head,head1);
+            break;            
         default:
            printf("Invalid option");
         }    
