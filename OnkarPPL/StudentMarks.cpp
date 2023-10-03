5ca47d080c912478d47d3294eafa4a7b6147f649
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-10-03 21:57:15 +0530

    Update StudentMarks.cpp

--- OnkarPPL/StudentMarks.cpp
+++ OnkarPPL/StudentMarks.cpp
@@ -3,47 +3,54 @@
 using namespace std;
 
 class Student{
-	public:
+	
+    public:
 
 	char name[10];
 	float marks[5];	
-	
-	void sname(){	
-		cout<<"\n\nEnter name: ";
-		cin>>name;
-	}
-	
-	void inmarks(){
-	    int i;
-	   cout<<endl<<"Enter marks: ";
-	    for(i=0;i<5;i++){
-            cin>>marks[i];
-	    }
-	}
-	
-	float percentage(){
-		int i;
-		float total=0,per;
-	  for(i=0;i<5;i++){
-	  	total=total+marks[i];
-	  }	
-	  per=total/5;
-	  return per;
-	}
-	
-	void display(){
-	 cout<<name<<"\t"<<percentage()<<"%"<<endl; 
-	}
-	Student(){
-	    sname();
-	    inmarks();
-	    percentage();
-	}
+	Student();
+	void sname();
+	void inmarks();
+	float percentage();
+	void display();
 };
 
+void Student::sname(){
+	cout<<"\nEnter name: ";
+	cin>>name;
+}
+
+void Student::inmarks(){
+    int i;
+	cout<<"Enter marks: ";
+	for(i=0;i<5;i++){
+        cin>>marks[i];
+	}
+}
+
+float Student::percentage(){
+	int i;
+	float total=0,per;
+	for(i=0;i<5;i++){
+	    total=total+marks[i];
+	}	
+	per=total/5;
+	return per;
+}
+
+void Student::display(){
+    cout<<name<<"\t"<<percentage()<<"%"<<endl; 
+}
+//Student constructor automatically calls function to get marks, calculate percentage
+Student::Student(){
+	sname();
+	inmarks();
+	percentage();
+}
+
 int main() {
-	Student s1,s2,s3,s4,s5;
-	cout<<"Marks list\nName\tmarks\n";
+	Student s1,s2,s3,s4,s5; //created 5 student objects
+	cout<<"\nMarks list\nName\tmarks\n";
     s1.display();
     s2.display();
     s3.display();
